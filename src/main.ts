6dfcdc5e664c47f1ac25a952c5fd678b345c355f
nitesh-dev <niteshdev547@gmail.com>
2023-04-05 20:32:02 +0530

    linked list DSA completed

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,34 @@
 import { LinkedList, ListNode } from "./LinkedList";
 
-let linkedList =new  LinkedList<string>()
+namespace Test {
 
-linkedList.push(new ListNode("hello node 1"))
-linkedList.push(new ListNode("hello node 2"))
-linkedList.push(new ListNode("hello node 3"))
-linkedList.push(new ListNode("hello node 4"))
-linkedList.push(new ListNode("hello node 5"))
+    export function runLinkedList() {
+        let linkedList = new LinkedList<string>()
 
-linkedList.printNodes()
+        linkedList.push(new ListNode("hello node 1"))
+        linkedList.push(new ListNode("hello node 2"))
+        linkedList.push(new ListNode("hello node 3"))
+        linkedList.push(new ListNode("hello node 4"))
+        linkedList.push(new ListNode("hello node 5"))
 
-console.log(linkedList.indexOf(new ListNode("hello node 1")))
-console.log(linkedList.isExist(new ListNode("hello node 2")))
+        linkedList.printNodes()
 
-linkedList.printNodes()
+        console.log(linkedList.pop())
+        console.log(linkedList.insertAt(1, new ListNode("hello 6")))
+
+        console.log(linkedList.indexOf(new ListNode("hello node 1")))
+        console.log(linkedList.isExist(new ListNode("hello node 2")))
+
+        linkedList.printNodes()
+    }
+
+    export function runBinaryTree(){
+        
+    }
+
+}
+
+//Test.runLinkedList()
+
+Test.runBinaryTree()
 
